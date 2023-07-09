af9a786879f79a803bf3be833ac183908ba7701d
Cera Glassburn <ceraglassburn@kaplanstudent.onmicrosoft.com>
2023-07-09 16:42:53 -0400

    Cleaned up GUI to match wireframe

--- ThriftyTrak/MainForm.Designer.cs
+++ ThriftyTrak/MainForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.lblTitle = new System.Windows.Forms.Label();
             this.labelUsername = new System.Windows.Forms.Label();
             this.btnDash = new System.Windows.Forms.Button();
@@ -44,136 +45,173 @@
             this.label2 = new System.Windows.Forms.Label();
             this.btnSellItem = new System.Windows.Forms.Button();
             this.btnEditSale = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblTitle
             // 
             this.lblTitle.AutoSize = true;
-            this.lblTitle.Location = new System.Drawing.Point(51, 13);
+            this.lblTitle.Font = new System.Drawing.Font("Berlin Sans FB", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(100, 28);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(58, 13);
+            this.lblTitle.Size = new System.Drawing.Size(138, 30);
             this.lblTitle.TabIndex = 0;
             this.lblTitle.Text = "ThriftyTrak";
             // 
             // labelUsername
             // 
             this.labelUsername.AutoSize = true;
-            this.labelUsername.Location = new System.Drawing.Point(710, 13);
+            this.labelUsername.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUsername.Location = new System.Drawing.Point(1008, 36);
             this.labelUsername.Name = "labelUsername";
-            this.labelUsername.Size = new System.Drawing.Size(85, 13);
+            this.labelUsername.Size = new System.Drawing.Size(129, 21);
             this.labelUsername.TabIndex = 1;
-            this.labelUsername.Text = "Hello, Username";
+            this.labelUsername.Text = "Hello, Username!";
             // 
             // btnDash
             // 
-            this.btnDash.Location = new System.Drawing.Point(12, 71);
+            this.btnDash.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(176)))), ((int)(((byte)(80)))));
+            this.btnDash.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDash.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDash.ForeColor = System.Drawing.Color.White;
+            this.btnDash.Location = new System.Drawing.Point(12, 96);
             this.btnDash.Name = "btnDash";
-            this.btnDash.Size = new System.Drawing.Size(75, 23);
+            this.btnDash.Size = new System.Drawing.Size(150, 60);
             this.btnDash.TabIndex = 2;
             this.btnDash.Text = "Dashboard";
-            this.btnDash.UseVisualStyleBackColor = true;
+            this.btnDash.UseVisualStyleBackColor = false;
             this.btnDash.Click += new System.EventHandler(this.btnDash_Click);
             // 
             // btnDonate
             // 
-            this.btnDonate.Location = new System.Drawing.Point(12, 100);
+            this.btnDonate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(153)))));
+            this.btnDonate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDonate.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDonate.ForeColor = System.Drawing.Color.White;
+            this.btnDonate.Location = new System.Drawing.Point(12, 162);
             this.btnDonate.Name = "btnDonate";
-            this.btnDonate.Size = new System.Drawing.Size(75, 23);
+            this.btnDonate.Size = new System.Drawing.Size(150, 60);
             this.btnDonate.TabIndex = 3;
             this.btnDonate.Text = "Donations";
-            this.btnDonate.UseVisualStyleBackColor = true;
+            this.btnDonate.UseVisualStyleBackColor = false;
             this.btnDonate.Click += new System.EventHandler(this.btnDonate_Click);
             // 
             // btnSales
             // 
-            this.btnSales.Location = new System.Drawing.Point(12, 130);
+            this.btnSales.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(153)))));
+            this.btnSales.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSales.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSales.ForeColor = System.Drawing.Color.White;
+            this.btnSales.Location = new System.Drawing.Point(12, 228);
             this.btnSales.Name = "btnSales";
-            this.btnSales.Size = new System.Drawing.Size(75, 23);
+            this.btnSales.Size = new System.Drawing.Size(150, 60);
             this.btnSales.TabIndex = 4;
             this.btnSales.Text = "Sales";
-            this.btnSales.UseVisualStyleBackColor = true;
+            this.btnSales.UseVisualStyleBackColor = false;
             this.btnSales.Click += new System.EventHandler(this.btnSales_Click);
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(12, 160);
+            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(153)))));
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(12, 294);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.Size = new System.Drawing.Size(150, 60);
             this.btnAdd.TabIndex = 5;
             this.btnAdd.Text = "Add";
-            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.UseVisualStyleBackColor = false;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnEdit
             // 
-            this.btnEdit.Location = new System.Drawing.Point(12, 218);
+            this.btnEdit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(153)))));
+            this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEdit.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.ForeColor = System.Drawing.Color.White;
+            this.btnEdit.Location = new System.Drawing.Point(12, 360);
             this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.Size = new System.Drawing.Size(150, 60);
             this.btnEdit.TabIndex = 6;
-            this.btnEdit.Text = "Edit Item";
-            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = false;
             this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(12, 276);
+            this.btnSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(153)))));
+            this.btnSearch.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearch.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.ForeColor = System.Drawing.Color.White;
+            this.btnSearch.Location = new System.Drawing.Point(12, 426);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.Size = new System.Drawing.Size(150, 60);
             this.btnSearch.TabIndex = 7;
             this.btnSearch.Text = "Search";
-            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.UseVisualStyleBackColor = false;
             this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
             // btnLogout
             // 
-            this.btnLogout.Location = new System.Drawing.Point(12, 306);
+            this.btnLogout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogout.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogout.ForeColor = System.Drawing.Color.White;
+            this.btnLogout.Location = new System.Drawing.Point(12, 492);
             this.btnLogout.Name = "btnLogout";
-            this.btnLogout.Size = new System.Drawing.Size(75, 23);
+            this.btnLogout.Size = new System.Drawing.Size(150, 60);
             this.btnLogout.TabIndex = 8;
             this.btnLogout.Text = "Log Out";
-            this.btnLogout.UseVisualStyleBackColor = true;
+            this.btnLogout.UseVisualStyleBackColor = false;
             this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
             // 
             // lblBanner
             // 
-            this.lblBanner.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.lblBanner.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.lblBanner.Location = new System.Drawing.Point(93, 71);
+            this.lblBanner.BackColor = System.Drawing.Color.Gray;
+            this.lblBanner.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBanner.ForeColor = System.Drawing.Color.White;
+            this.lblBanner.Location = new System.Drawing.Point(171, 96);
             this.lblBanner.Name = "lblBanner";
-            this.lblBanner.Size = new System.Drawing.Size(1044, 23);
+            this.lblBanner.Size = new System.Drawing.Size(966, 60);
             this.lblBanner.TabIndex = 9;
             this.lblBanner.Text = "Welcome!";
             this.lblBanner.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // listBox1
             // 
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(118, 135);
+            this.listBox1.Location = new System.Drawing.Point(171, 182);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(1002, 147);
+            this.listBox1.Size = new System.Drawing.Size(966, 225);
             this.listBox1.TabIndex = 10;
             // 
             // listBox2
             // 
+            this.listBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.Location = new System.Drawing.Point(118, 332);
+            this.listBox2.Location = new System.Drawing.Point(171, 426);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(1002, 160);
+            this.listBox2.Size = new System.Drawing.Size(966, 225);
             this.listBox2.TabIndex = 11;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(118, 109);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(168, 166);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.Size = new System.Drawing.Size(64, 13);
             this.label1.TabIndex = 12;
-            this.label1.Text = "Current Inventory:";
+            this.label1.Text = "Donations:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(118, 306);
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(168, 410);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(36, 13);
             this.label2.TabIndex = 13;
@@ -181,29 +219,43 @@
             // 
             // btnSellItem
             // 
-            this.btnSellItem.Location = new System.Drawing.Point(12, 189);
+            this.btnSellItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnSellItem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSellItem.Location = new System.Drawing.Point(68, 584);
             this.btnSellItem.Name = "btnSellItem";
-            this.btnSellItem.Size = new System.Drawing.Size(75, 23);
+            this.btnSellItem.Size = new System.Drawing.Size(50, 50);
             this.btnSellItem.TabIndex = 14;
             this.btnSellItem.Text = "Sell Item";
-            this.btnSellItem.UseVisualStyleBackColor = true;
+            this.btnSellItem.UseVisualStyleBackColor = false;
             this.btnSellItem.Click += new System.EventHandler(this.button1_Click);
             // 
             // btnEditSale
             // 
-            this.btnEditSale.Location = new System.Drawing.Point(12, 247);
+            this.btnEditSale.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnEditSale.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEditSale.Location = new System.Drawing.Point(12, 584);
             this.btnEditSale.Name = "btnEditSale";
-            this.btnEditSale.Size = new System.Drawing.Size(75, 23);
+            this.btnEditSale.Size = new System.Drawing.Size(50, 50);
             this.btnEditSale.TabIndex = 15;
             this.btnEditSale.Text = "Edit Sale";
-            this.btnEditSale.UseVisualStyleBackColor = true;
+            this.btnEditSale.UseVisualStyleBackColor = false;
             this.btnEditSale.Click += new System.EventHandler(this.btnEditSale_Click);
+            // 
+            // label3
+            // 
+            this.label3.Image = ((System.Drawing.Image)(resources.GetObject("label3.Image")));
+            this.label3.Location = new System.Drawing.Point(9, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 85);
+            this.label3.TabIndex = 16;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1149, 533);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1149, 663);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.btnEditSale);
             this.Controls.Add(this.btnSellItem);
             this.Controls.Add(this.label2);
@@ -221,7 +273,7 @@
             this.Controls.Add(this.labelUsername);
             this.Controls.Add(this.lblTitle);
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Text = "Dashboard";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -245,6 +297,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnSellItem;
         private System.Windows.Forms.Button btnEditSale;
+        private System.Windows.Forms.Label label3;
     }
 }
 
